7c62a4b05fc44b40843fc9f4c14d22ba13821c87
wrongwrong <boranti1995@gmail.com>
2025-05-11 19:52:42 +0900

    Add value class tests for primitive that consume two argument slots
    from https://github.com/ProjectMapK/jackson-module-kogera/pull/343

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/kogeraIntegration/deser/valueClass/defaultArgument/TwoUnitPrimitiveTest.kt
@@ -0,0 +1,38 @@
+package com.fasterxml.jackson.module.kotlin.kogeraIntegration.deser.valueClass.defaultArgument
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.module.kotlin.defaultMapper
+import com.fasterxml.jackson.module.kotlin.kogeraIntegration.deser.valueClass.TwoUnitPrimitive
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TwoUnitPrimitiveTest {
+    data class ByConstructor(
+        val nn: TwoUnitPrimitive = TwoUnitPrimitive(1),
+        val nNn: TwoUnitPrimitive? = TwoUnitPrimitive(2),
+        val nN: TwoUnitPrimitive? = null
+    )
+
+    @Test
+    fun byConstructorTest() {
+        assertEquals(ByConstructor(), defaultMapper.readValue<ByConstructor>("{}"))
+    }
+
+    data class ByFactory(val nn: TwoUnitPrimitive, val nNn: TwoUnitPrimitive?, val nN: TwoUnitPrimitive?) {
+        companion object {
+            @JvmStatic
+            @JsonCreator
+            fun creator(
+                nn: TwoUnitPrimitive = TwoUnitPrimitive(1),
+                nNn: TwoUnitPrimitive? = TwoUnitPrimitive(2),
+                nN: TwoUnitPrimitive? = null
+            ) = ByFactory(nn, nNn, nN)
+        }
+    }
+
+    @Test
+    fun byFactoryTest() {
+        assertEquals(ByFactory.creator(), defaultMapper.readValue<ByFactory>("{}"))
+    }
+}
