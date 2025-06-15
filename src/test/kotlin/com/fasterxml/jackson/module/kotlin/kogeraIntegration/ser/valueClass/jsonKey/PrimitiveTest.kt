605e34cafa8229d022e00070bbf9a157d1c0544f
wrongwrong <boranti1995@gmail.com>
2025-06-15 13:17:27 +0900

    Add exhaustive tests for JsonValue and JsonKey usage in value class
    from https://github.com/ProjectMapK/jackson-module-kogera/pull/358

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/kogeraIntegration/ser/valueClass/jsonKey/PrimitiveTest.kt
@@ -0,0 +1,37 @@
+package com.fasterxml.jackson.module.kotlin.kogeraIntegration.ser.valueClass.jsonKey
+
+import com.fasterxml.jackson.annotation.JsonKey
+import com.fasterxml.jackson.module.kotlin.defaultMapper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PrimitiveTest {
+    @JvmInline
+    value class NonNull(val v: Int) {
+        @JsonKey
+        fun jsonValue() = v + 100
+    }
+
+    @Test
+    fun nonNullTest() {
+        assertEquals(
+            """{"100":null}""",
+            defaultMapper.writeValueAsString(mapOf(NonNull(0) to null)),
+        )
+    }
+
+    @JvmInline
+    value class Nullable(val v: Int) {
+        @JsonKey
+        fun jsonValue() = v.takeIf { it % 2 == 0 }?.let { it + 100 }
+    }
+
+    // The case of returning null as a key is unnecessary because it will result in an error
+    @Test
+    fun nullableTest() {
+        assertEquals(
+            """{"100":null}""",
+            defaultMapper.writeValueAsString(mapOf(Nullable(0) to null)),
+        )
+    }
+}
---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/kogeraIntegration/ser/valueClass/jsonValue/NullablePrimitiveTest.kt
@@ -0,0 +1,55 @@
+package com.fasterxml.jackson.module.kotlin.kogeraIntegration.ser.valueClass.jsonValue
+
+import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.module.kotlin.defaultMapper
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class NullablePrimitiveTest {
+    @JvmInline
+    value class Value(val v: Int?) {
+        @JsonValue
+        fun jsonValue() = v?.let { it + 100 }
+    }
+
+    @Nested
+    inner class DirectTest {
+        @Test
+        fun nonNull() {
+            assertEquals(
+                "100",
+                defaultMapper.writeValueAsString(Value(0)),
+            )
+        }
+
+        @Test
+        fun `null`() {
+            assertEquals(
+                "null",
+                defaultMapper.writeValueAsString(Value(null)),
+            )
+        }
+    }
+
+    data class Dto(val v: Value)
+
+    @Nested
+    inner class AsPropertyTest {
+        @Test
+        fun nonNull() {
+            assertEquals(
+                """{"v":100}""",
+                defaultMapper.writeValueAsString(Dto(Value(0))),
+            )
+        }
+
+        @Test
+        fun `null`() {
+            assertEquals(
+                """{"v":null}""",
+                defaultMapper.writeValueAsString(Dto(Value(null))),
+            )
+        }
+    }
+}
