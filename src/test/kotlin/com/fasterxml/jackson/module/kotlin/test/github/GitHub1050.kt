e75575af998573bc6cb4f6c32010bdf3815bf56e
wrongwrong <boranti1995@gmail.com>
2025-09-14 14:19:05 +0900

    Added deserialization tests for value classes with private constructor
    wrt #1050

---
+++ src/test/kotlin/com/fasterxml/jackson/module/kotlin/test/github/GitHub1050.kt
@@ -0,0 +1,24 @@
+package com.fasterxml.jackson.module.kotlin.test.github
+
+import com.fasterxml.jackson.module.kotlin.defaultMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.Locale
+
+class GitHub1050Test {
+    @JvmInline
+    value class PortCode private constructor(val value: String) : Comparable<PortCode> {
+        override fun compareTo(other: PortCode): Int { TODO("Not yet implemented") }
+
+        companion object {
+            operator fun invoke(value: String) = PortCode(value.uppercase(Locale.getDefault()))
+        }
+    }
+
+    @Test
+    fun test() {
+        val result = defaultMapper.readValue<PortCode>("\"ABC\"")
+        assertEquals("ABC", result.value)
+    }
+}
